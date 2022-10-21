d5bb14dbafcb9d25e5beff2cbb640e27f58fb89f
eli4sDM <eliasdomin2003@gmail.com>
2022-10-21 15:46:13 -0300

    tira error

---
+++ album/src/App.jsx
@@ -0,0 +1,22 @@
+
+//importamos los componentes de interface
+import Navbar from './components/Navbar'
+/* import Carrousel from './components/Carrousel'
+import Marketing from './components/Marketing'*/
+import Main from './Main'
+
+
+function App() {
+  return (
+    
+    <>
+      <Navbar/>
+      
+
+      <Main/>
+    </>
+
+  )
+}
+
+export default App
